ab3dfa0df1e1d88c8cac70940496fb61d94804bd
Jake Heiser <jakeheiser1@gmail.com>
2017-08-29 17:27:24 -0500

    Open xc project after creating

--- Sources/CLI/Xc.swift
+++ Sources/CLI/Xc.swift
@@ -5,6 +5,7 @@
 //  Created by Jake Heiser on 7/22/17.
 //
 
+import Foundation
 import SwiftCLI
 import Core
 
@@ -12,8 +13,21 @@ class XcCommand: Command {
     
     let name = "xc"
     
+    let noOpen = Flag("-n", "--no-open")
+    
     func execute() throws {
         try SPM().generateXcodeProject()
+        
+        if noOpen.value {
+            return
+        }
+        
+        let package = try Package.load(directory: ".")
+        let open = Process()
+        open.launchPath = "/usr/bin/env"
+        open.arguments = ["open", "\(package.name).xcodeproj"]
+        open.launch()
+        open.waitUntilExit()
     }
     
 }
