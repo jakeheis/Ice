6caa7db519337e2bf03b982d1310fe565faabeeb
Jake Heiser <jakeheiser1@gmail.com>
2017-08-30 08:14:00 -0500

    Toying with idea of output translator

---
+++ Sources/CLI/OutputTranslator.swift
@@ -0,0 +1,21 @@
+//
+//  OutputTranslator.swift
+//  CLI
+//
+//  Created by Jake Heiser on 8/30/17.
+//
+
+import Foundation
+import Regex
+
+class OutputTranslator {
+    
+    init(setup: (_ translator: OutputTranslator) -> ()) {
+        
+    }
+    
+    func on(_ match: String, yield: (_ match: String) -> String) {
+        
+    }
+    
+}
