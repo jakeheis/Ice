e91b3484d0ce87bbf600c02f293c95e82ab94a54
Jake Heiser <jakeheiser1@gmail.com>
2017-09-05 18:59:27 -0500

    Beautify ice test

--- Sources/Core/OutputTransformer.swift
+++ Sources/Core/OutputTransformer.swift
@@ -11,43 +11,15 @@ import CLISpinner
 import Dispatch
 import SwiftCLI
 
-public class OutputTransformer {
+class Hose {
     
-    public typealias Translation = (_ captures: [String]) -> String
-    public typealias SpinnerDone = (_ spinner: Spinner, _ capture: [String]) -> ()
-    
-    public enum Stream {
-        case out
-        case err
-        
-        func output(_ text: String) {
-            switch self {
-            case .out: print(text)
-            case .err: printError(text)
-            }
-        }
-    }
-    
-    let output: Pipe
-    let error: Pipe
-    
-    private var prefix: String? = nil
-    private var suffix: String? = nil
-    
-    private var responseGenerators: [ResponseGenerator] = []
-    private var currentResponse: Response?
+    private let pipe: Pipe
+    var onLine: ((_ line: String) -> ())?
     
     init() {
-        self.output = Pipe()
-        self.error = Pipe()
-        
-        output.fileHandleForReading.readabilityHandler = readability(stream: .out)
-        error.fileHandleForReading.readabilityHandler = readability(stream: .err)
-    }
-    
-    private func readability(stream: Stream) -> (FileHandle) -> () {
-        return { (handle) in
-            guard let str = String(data:handle.availableData, encoding: .utf8), !str.isEmpty else {
+        self.pipe = Pipe()
+        self.pipe.fileHandleForReading.readabilityHandler = { (handle) in
+            guard let str = String(data: handle.availableData, encoding: .utf8), !str.isEmpty else {
                 return
             }
             
@@ -56,30 +28,106 @@ public class OutputTransformer {
                 if let last = lines.last, last.isEmpty {
                     lines.removeLast()
                 }
-                
-                self.currentResponse?.stop()
-                self.currentResponse = nil
-                for (index, line) in lines.enumerated() {
-                    self.respond(stream: stream, line: line, stopImmediately: index < lines.endIndex - 1)
+                for line in lines {
+                    self.onLine?(line)
                 }
             }
         }
+    }
+    
+    func attachStdout(_ process: Process) {
+        process.standardOutput = pipe
+    }
+    
+    func attachStderr(_ process: Process) {
+        process.standardError = pipe
+    }
+    
+}
+
+public enum Stream {
+    case out
+    case err
+    
+    func output(_ text: String, terminator: String = "\n") {
+        switch self {
+        case .out: print(text, terminator: terminator)
+        case .err: printError(text, terminator: terminator)
+        }
+    }
+}
+
+public class OutputTransformer {
+    
+    public typealias SpinnerDone = (_ spinner: Spinner, _ capture: [String]) -> ()
+    
+    let out: Hose
+    let error: Hose
+    
+    private var prefix: String? = nil
+    private var suffix: String? = nil
+    
+    private var outGenerators: [ResponseGenerator] = []
+    private var errorGenerators: [ResponseGenerator] = []
+    
+    private var currentResponse: Response?
+    
+    init() {
+        self.out = Hose()
+        self.error = Hose()
+        
+        self.out.onLine = { [weak self] in self?.readLine(stream: .out, line: $0) }
+        self.error.onLine = { [weak self] in self?.readLine(stream: .err, line: $0) }
+    }
+    
+    private func readLine(stream: Stream, line: String) {
+        if let currentResponse = currentResponse {
+            if currentResponse.contine(on: line) {
+                return
+            }
+            currentResponse.stop()
+        }
+        let generators = stream == .out ? outGenerators : errorGenerators
+        for responseGenerator in generators {
+            if responseGenerator.matches(line) {
+                currentResponse = responseGenerator.generateResponse(to: line)
+                return
+            }
+        }
+        stream.output(line)
     }
     
     public func first(_ str: String) {
         self.prefix = str
     }
     
-    public func replace(_ matcher: StaticString, _ yield: @escaping Translation) {
-        responseGenerators.append(ResponseGenerator(matcher: matcher, replace: yield, stream: .out))
+    public func respond(on stream: Stream, with generator: ResponseGenerator) {
+        if stream == .out {
+            outGenerators.append(generator)
+        } else {
+            errorGenerators.append(generator)
+        }
     }
     
-    public func replaceErr(_ matcher: StaticString, _ yield: @escaping Translation) {
-        responseGenerators.append(ResponseGenerator(matcher: matcher, replace: yield, stream: .err))
+    public func replace(_ matcher: StaticString, on stream: Stream = .out, _ translation: @escaping CaptureTranslation) {
+        let generator = ResponseGenerator(matcher: matcher, generate: {
+            return ReplaceResponse(stream: stream, translation: translation)
+        })
+        respond(on: stream, with: generator)
     }
     
-    public func spin(_ matcher: StaticString, _ during: @escaping OutputTransformer.Translation, _ done: @escaping OutputTransformer.SpinnerDone, stream: Stream = .out) {
-        responseGenerators.append(ResponseGenerator(matcher: matcher, during: during, after: done, stream: .out))
+    public func spin(_ matcher: StaticString, _ during: @escaping CaptureTranslation, _ done: @escaping SpinnerResponse.Completion) {
+        let generator = ResponseGenerator(matcher: matcher, generate: {
+            return SpinnerResponse(during: during, after: done)
+        })
+        respond(on: .out, with: generator)
+    }
+    
+    public func ignore(_ matcher: StaticString, on stream: Stream = .out) {
+        let generator = ResponseGenerator(matcher: matcher) {
+            return IgnoreResponse()
+        }
+        respond(on: stream, with: generator)
     }
     
     public func last(_ str: String) {
@@ -87,22 +135,8 @@ public class OutputTransformer {
     }
     
     public func attach(_ process: Process) {
-        process.standardOutput = output
-        process.standardError = error
-    }
-    
-    private func respond(stream: Stream, line: String, stopImmediately: Bool) {
-        for responseGenerator in self.responseGenerators {
-            if responseGenerator.matches(stream, line) {
-                let response = responseGenerator.respond(line)
-                if stopImmediately {
-                    response?.stop()
-                }
-                currentResponse = response
-                return
-            }
-        }
-        stream.output(line)
+        out.attachStdout(process)
+        error.attachStderr(process)
     }
     
     func printPrefix() {
@@ -122,52 +156,93 @@ public class OutputTransformer {
     
 }
 
-private class ResponseGenerator {
+public class ResponseGenerator {
     private let regex: Regex
-    private let internalResponse: (_ captures: [String]) -> Response?
-    private let stream: OutputTransformer.Stream
+    private let generate: () -> Response
     
-    init(matcher: StaticString, replace: @escaping (_ captures: [String]) -> String, stream: OutputTransformer.Stream) {
+    init(matcher: StaticString, generate: @escaping () -> Response) {
         self.regex = Regex(matcher)
-        self.internalResponse = { (captures) in
-            stream.output(replace(captures))
-            return nil
-        }
-        self.stream = stream
+        self.generate = generate
     }
     
-    init(matcher: StaticString, during: @escaping (_ captures: [String]) -> String, after: @escaping (_ spinner: Spinner, _ captures: [String]) -> (), stream: OutputTransformer.Stream) {
-        self.regex = Regex(matcher)
-        self.internalResponse = { (captures) in
-            let spinner = Spinner(pattern: .dots, text: during(captures))
-            spinner.start()
-            return Response(spinner: spinner, after: after, captures: captures)
-        }
-        self.stream = stream
+    func matches(_ line: String) -> Bool {
+        return regex.matches(line)
     }
     
-    func matches(_ stream: OutputTransformer.Stream, _ line: String) -> Bool {
-        return stream == self.stream && regex.matches(line)
-    }
-    
-    func respond(_ line: String) -> Response? {
+    func generateResponse(to line: String) -> Response {
         let captures = regex.firstMatch(in: line)?.captures.flatMap { $0 } ?? []
-        return internalResponse(captures)
+        let response = generate()
+        response.go(captures: captures)
+        return response
     }
 }
 
-private class Response {
-    let spinner: Spinner
-    let after: (_ spinner: Spinner, _ captures: [String]) -> ()
-    let captures: [String]
+public protocol Response: class {
+    func go(captures: [String])
+    func contine(on line: String) -> Bool
+    func stop()
+}
+
+public typealias CaptureTranslation = (_ captures: [String]) -> String
+
+public class ReplaceResponse: Response {
     
-    init(spinner: Spinner, after: @escaping (_ spinner: Spinner, _ captures: [String]) -> (), captures: [String]) {
-        self.spinner = spinner
+    private let stream: Stream
+    private let translation: CaptureTranslation
+    
+    init(stream: Stream, translation: @escaping CaptureTranslation) {
+        self.stream = stream
+        self.translation = translation
+    }
+    
+    public func go(captures: [String]) {
+        stream.output(translation(captures))
+    }
+    
+    public func contine(on line: String) -> Bool {
+        return false
+    }
+    
+    public func stop() {}
+    
+}
+
+public class SpinnerResponse: Response {
+    
+    public typealias Completion = (_ spinner: Spinner, _ captures: [String]) -> ()
+
+    private let during: CaptureTranslation
+    private let after: Completion
+    
+    private var spinner: Spinner?
+    private var captures: [String] = []
+    
+    init(during: @escaping CaptureTranslation, after: @escaping Completion) {
+        self.during = during
         self.after = after
-        self.captures = captures
     }
     
-    func stop() {
-        after(spinner, captures)
+    public func go(captures: [String]) {
+        self.captures = captures
+        let spinner = Spinner(pattern: .dots, text: during(captures))
+        spinner.start()
+        self.spinner = spinner
     }
+    
+    public func contine(on line: String) -> Bool {
+        return false
+    }
+
+    public func stop() {
+        if let spinner = spinner {
+            after(spinner, captures)
+        }
+    }
+    
+}
+
+public class IgnoreResponse: Response {
+    public func go(captures: [String]) {}
+    public func contine(on line: String) -> Bool { return false }
+    public func stop() {}
 }
--- Sources/Core/SPM.swift
+++ Sources/Core/SPM.swift
@@ -8,6 +8,106 @@
 import Foundation
 import Files
 import CLISpinner
+import Rainbow
+import Regex
+
+class TestSuiteResponse: Response {
+    
+    static let caseRegex = Regex("^Test Case .* ([^ ]*)\\]' (started|passed|failed)")
+    static let doneRegex = Regex("Executed .* tests")
+    
+    let stream: Stream
+    var suiteName: String?
+    
+    var done = false
+    var failures: [String] = []
+    
+    init(stream: Stream) {
+        self.stream = stream
+    }
+    
+    func go(captures: [String]) {
+        suiteName = captures[0]
+        stream.output(badge(text: "RUNS", color: .blue), terminator: "")
+    }
+    
+    func contine(on line: String) -> Bool {
+        guard !done else {
+            return false
+        }
+        done = TestSuiteResponse.doneRegex.matches(line)
+        if let match = TestSuiteResponse.caseRegex.firstMatch(in: line) {
+            if match.captures[1] == "failed" {
+                if failures.isEmpty {
+                    stream.output("\r" + badge(text: "FAIL", color: .red))
+                    stream.output("")
+                }
+                failures.append(match.captures[0]!)
+                print(" ● \(match.captures[0]!)".red.bold)
+            }
+        }
+        return true
+    }
+    
+    func stop() {
+        if failures.isEmpty {
+            stream.output("\r" + badge(text: "PASS", color: .green))
+        } else {
+            stream.output("")
+        }
+    }
+    
+    func badge(text: String, color: BackgroundColor) -> String {
+        return " \(text) ".applyingBackgroundColor(color).black.bold + " " + suiteName!.bold
+    }
+    
+}
+
+class TestEndResponse: Response {
+    
+//    static let countRegex = Regex("Executed ([0-9]+) tests, with ([0-9]*) failure")
+    static let countRegex = Regex("Executed ([0-9]+) tests, with ([0-9]*) failures? .* \\(([\\.0-9]+)\\) seconds$")
+
+    let stream: Stream = .err
+    var nextLine = true
+    
+    func go(captures: [String]) {
+        stream.output("")
+    }
+    
+    func contine(on line: String) -> Bool {
+        guard nextLine else {
+            return false
+        }
+        nextLine = false
+        
+        if let countMatch = TestEndResponse.countRegex.firstMatch(in: line) {
+            if let totalStr = countMatch.captures[0], let totalCount = Int(totalStr),
+                let failedStr = countMatch.captures[1], let failedCount = Int(failedStr) {
+                
+                var parts: [String] = []
+                if failedCount > 0 {
+                    parts.append("\(failedCount) failed".bold.red)
+                }
+                if failedCount < totalCount {
+                    parts.append("\(totalCount - failedCount) passed".bold.green)
+                }
+                parts.append("\(totalCount) total")
+                
+                let output = "Tests:\t".bold.white + parts.joined(separator: ", ")
+                stream.output(output)
+            }
+            if let time = countMatch.captures[2] {
+                stream.output("Time:\t".bold.white + time + "s")
+            }
+        }
+        
+        return true
+    }
+    
+    func stop() {}
+    
+}
 
 public class SPM {
 
@@ -50,11 +150,23 @@ public class SPM {
     }
 
     public func test() throws {
-        try exec(arguments: ["test"]).execute(transform: { (t) in
-            t.spin("Compile Swift Module '(.*)'", { "Compiling " + $0[0] }, { $0.succeed(text: "Compiled " + $1[0]) })
-            t.replaceErr("Test Case .* ([^ ]*)\\]' started", { $0[0] + " started" })
-            t.replaceErr("Test Case .* ([^ ]*)\\]' passed", { $0[0] + " passed" })
-        })
+        do {
+            try exec(arguments: ["test"]).execute(transform: { (t) in
+                t.spin("Compile Swift Module '(.*)'", { "Compiling " + $0[0] }, { $0.succeed(text: "Compiled " + $1[0]) })
+                t.ignore("^Test Suite 'All tests' started", on: .err)
+                t.replace("^Test Suite '(.*)\\.xctest' started", on: .err, { "\n\($0[0]):\n".dim })
+                t.respond(on: .err, with: ResponseGenerator(matcher: "^Test Suite 'All tests' (passed|failed)", generate: {
+                    return TestEndResponse()
+                }))
+                t.respond(on: .err, with: ResponseGenerator(matcher: "^Test Suite '(.*)'", generate: {
+                    return TestSuiteResponse(stream: .err)
+                }))
+                t.ignore(".*", on: .out)
+                t.last("\n")
+            })
+        } catch let error as Exec.Error {
+            throw IceError(exitStatus: error.exitStatus)
+        }
     }
 
     public func generateXcodeProject() throws {
