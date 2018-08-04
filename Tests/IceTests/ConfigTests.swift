a38c896d61f85d77a77da4b5df3afb883f68b60c
Jake Heiser <jakeheiser1@gmail.com>
2018-08-04 18:02:08 -0500

    Tests run on Linux

--- Tests/IceTests/ConfigTests.swift
+++ Tests/IceTests/ConfigTests.swift
@@ -58,6 +58,7 @@ class ConfigTests: XCTestCase {
         Valid keys:
         
           reformat      whether Ice should organize your Package.swift (alphabetize, etc.); defaults to false
+          openAfterXc   whether Ice should open Xcode the generated project after running `ice xc`; defaults to true
 
         
         """)
