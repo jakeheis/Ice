cf15fb43643d847d7a78f1912f31782d77d7b235
Jake Heiser <jakeheiser1@gmail.com>
2019-12-27 08:10:48 -0600

    Update PathKit

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jakeheis/Icebox", from: "0.0.6"),
-        .package(url: "https://github.com/kylef/PathKit", from: "0.9.0"),
+        .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.1"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.1"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable", from: "0.9.0"),
